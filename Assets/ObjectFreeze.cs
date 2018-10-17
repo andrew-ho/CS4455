b2cb595bd69454576046b391c7dbe2c0d67e1558
Andrew Ho <aho3555@gmail.com>
2018-10-17 19:44:18 -0400

    second commit

---
+++ Assets/ObjectFreeze.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFreeze : MonoBehaviour, TimeStop {
+    public bool stopped = false;
+    public float timer = 0f;
+
+    public void Stop()
+    {
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            timer = 0f;
+            stopped = false;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stopped)
+        {
+            Stop();
+            return;
+        }
+        else
+        {
+
+        }
+	}
+}

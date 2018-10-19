291957d570a3e8ed67dc5c1083e07d49aba9ae24
Andrew Ho <aho3555@gmail.com>
2018-10-19 15:58:02 -0400

    added object freezing

---
+++ Assets/Scripts/GamePlay/ObjectFreeze.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFreeze : MonoBehaviour {
+    public bool stopped = false;
+    public float timer = 0f;
+    public Rigidbody rb;
+    public void Stop()
+    {
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            timer = 0f;
+            rb.constraints = RigidbodyConstraints.None;
+            stopped = false;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (stopped)
+        {
+            Stop();
+            return;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+                stopped = true;
+            }
+        }
+    }
+}

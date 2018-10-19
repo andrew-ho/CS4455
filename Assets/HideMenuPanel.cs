f07fa86cc8347176e4a9f40fc330e6c753c98c81
Bang <bangpham2701@gmail.com>
2018-10-19 18:47:29 -0400

    added instruction menu

---
+++ Assets/HideMenuPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideMenuPanel : MonoBehaviour {
+
+	public GameObject MenuPanel;
+	public GameObject InstructionPanel;
+
+	public void showMenu() {
+		MenuPanel.gameObject.SetActive(true);
+	}
+
+	public void hideMenu() {
+		MenuPanel.gameObject.SetActive(false);
+	}
+
+
+	public void showInstruction() {
+		InstructionPanel.gameObject.SetActive(true);
+	}
+
+	public void hideInstruction() {
+		InstructionPanel.gameObject.SetActive(false);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

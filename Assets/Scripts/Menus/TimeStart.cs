c28964b0c74291698d2ee8dd645843e8a21ed2b8
annie <atruong32@gatech.edu>
2018-10-26 16:20:43 -0400

    fixed time

---
+++ Assets/Scripts/Menus/TimeStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeStart : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+
+    }
+}

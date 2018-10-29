75ce9e88c1cb5327bcd00dd0fe9eb3ab82771b4a
annie <atruong32@gatech.edu>
2018-10-29 16:45:33 -0400

    added scene level_01 and update the restart script accordingly

--- Assets/Scripts/Menus/GameStart.cs
+++ Assets/Scripts/Menus/GameStart.cs
@@ -7,6 +7,17 @@ public class GameStart : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        Scene curr_Scene = SceneManager.GetActiveScene();
+        if (curr_Scene.name == "Level_01") {
+            SceneManager.LoadScene("Level_01");
+        } else if (curr_Scene.name == "Level_02") {
+            SceneManager.LoadScene("Level_02");
+        } else if (curr_Scene.name == "Level_03") {
+            SceneManager.LoadScene("Level_03");
+        } else if (curr_Scene.name == "Level_04") {
+            SceneManager.LoadScene("Level_04");
+        } else {
+            SceneManager.LoadScene("SampleScene");
+        } 
     }
 }

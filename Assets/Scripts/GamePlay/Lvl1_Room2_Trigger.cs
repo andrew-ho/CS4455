d85e6259678448cc16d6ae9f4c131812515d6db8
AnT <atruong32@gatech.edu>
2018-11-01 18:45:55 -0400

    completed UI for lvl01

---
+++ Assets/Scripts/GamePlay/Lvl1_Room2_Trigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl1_Room2_Trigger : MonoBehaviour {
+
+    GameObject block_plane;
+    Collider player;
+
+
+	// Use this for initialization
+	void Start () {
+        block_plane = GameObject.Find("block_plane");
+        player = GameObject.Find("vThirdPersonCamera").GetComponent<Collider>();
+    }
+	
+	// Update is called once per frame
+    //void Update () 
+    //{
+    //}
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        //Debug.Log("triggered!!!!!!!!!!!!!!!!");
+        //Debug.Log(obj);
+        //Debug.Log(player);
+        //if (obj == player) {
+        block_plane.SetActive(false);
+        //Debug.Log("triggered.............");
+        //}
+    }
+}

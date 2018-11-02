021e4b2482c554c271435f7bf31ce3ecb2777655
Andrew Ho <aho3555@gmail.com>
2018-11-01 21:13:28 -0400

    Added next level thing

---
+++ Assets/Scripts/GamePlay/SceneManagement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagement : MonoBehaviour {
+    CanvasGroup canvasGroup;
+    public void Start()
+    {
+        canvasGroup = GameObject.Find("NextLevelPanel").GetComponent<CanvasGroup>();
+    }
+
+	public void OnCollisionEnter(Collision col)
+    {
+        GameObject.Find("vThirdPersonCamera").SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+        Time.timeScale = 0f;
+    }
+}

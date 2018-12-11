ae35be4177abb9e99d9a0a7648f7008f92dc70a7
Bang <bangpham2701@gmail.com>
2018-12-10 21:26:15 -0500

    added menu hover

---
+++ Assets/Scripts/Menus/ButtonHover.cs
@@ -0,0 +1,22 @@
+﻿ using UnityEngine;  
+ using System.Collections;  
+ using UnityEngine.EventSystems;  
+ using UnityEngine.UI;
+ 
+ public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Color newColor = new Color(255, 159, 0);
+    public Text text;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("hover");
+        text.color = newColor; //Or however you do your color
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = Color.white; //Or however you do your color
+    }
+
+}

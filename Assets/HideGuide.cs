35801476623c4b566312cebb7a67af4f049de4b7
Bang <bangpham2701@gmail.com>
2018-12-10 23:16:03 -0500

    added toggle to hide guide when pause

---
+++ Assets/HideGuide.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+
+public class HideGuide : MonoBehaviour
+{
+    bool isHidden;
+    CanvasGroup canvasGroup;
+
+    void Awake()
+    {
+        //GameObject camera = GameObject.Find("PauseCamera");
+        //camera.SetActive(true);
+        canvasGroup = GetComponent<CanvasGroup>();
+        isHidden = false;
+
+    }
+    // Update is called once per frame
+        void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape)) {
+
+            hide();
+        }
+    }
+
+    public void hide() {
+
+        if (isHidden == false)
+            {
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false; 
+                canvasGroup.alpha = 0f;
+                isHidden = true;
+            }   
+            else if (isHidden == true)
+            {
+                canvasGroup.interactable = true; 
+                canvasGroup.blocksRaycasts = true; 
+                canvasGroup.alpha = 1f;
+                 isHidden = false;
+            }
+        
+
+    }
+
+}

7d3a28efe7ad2c947b753173743fc5290ee72bca
Jacob Light <jakestar0306@comcast.net>
2018-12-11 13:02:15 -0500

    Revert "Finished more of Level 1 (part 1 of push)"
    This reverts commit 26b813a6f395f16476f99e09c526be47c879caf1.

--- Assets/Scripts/GamePlay/Magnet.cs
+++ Assets/Scripts/GamePlay/Magnet.cs
@@ -3,84 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Magnet : MonoBehaviour {
-    //Vector3 objectPos;
+    Vector3 objectPos;
     float distance;
 
     public bool canHold = true;
     GameObject item;
     GameObject tempParent;
     public bool isHolding = false;
-    private bool wasHolding = false;
     Camera cam;
     public bool carry;
     public float dis;
     public float smooth;
     public GameObject carriedObject;
-
-    private bool startingGravity;
-    private Transform startingParent;
-    private Rigidbody rb;
-    private Vector3 pos;
-    private Vector3 rot;
-
     // Use this for initialization
     void Start () {
         item = this.gameObject;
         cam = GameObject.Find("vThirdPersonController").GetComponent<Camera>();
         tempParent = GameObject.Find("Guide");
-
-        rb = item.GetComponent<Rigidbody>();
-        startingGravity = item.GetComponent<Rigidbody>().useGravity;
-        startingParent = item.transform.parent;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (isHolding)
         {
-            if (!wasHolding) {
-                pos = item.transform.position;
-                rot = transform.eulerAngles;
-
-                if (item.name == "Bottom Support Rod") {
-                    rb.constraints = rb.constraints & ~RigidbodyConstraints.FreezePositionX;
-                }
-            }
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             item.transform.SetParent(tempParent.transform);
-            rb.useGravity = false;
-            if ((rb.constraints & RigidbodyConstraints.FreezePositionX) != RigidbodyConstraints.None) {
-                transform.position = new Vector3(pos.x, transform.position.y, transform.position.z);
-            }
-            if ((rb.constraints & RigidbodyConstraints.FreezePositionY) != RigidbodyConstraints.None) {
-                transform.position = new Vector3(transform.position.x, pos.y, transform.position.z);
-            }
-            if ((rb.constraints & RigidbodyConstraints.FreezePositionZ) != RigidbodyConstraints.None) {
-                transform.position = new Vector3(transform.position.x, transform.position.y, pos.z);
-            }
-            if ((rb.constraints & RigidbodyConstraints.FreezeRotation) != RigidbodyConstraints.None) {
-                transform.eulerAngles = rot;
-            }
+            item.GetComponent<Rigidbody>().useGravity = false;
         }
         else
         {
-            //objectPos = item.transform.position;
-            item.transform.SetParent(startingParent);
-            rb.useGravity = startingGravity;
-
-            if (item.name == "Bottom Support Rod") {
-                rb.constraints = rb.constraints | RigidbodyConstraints.FreezePositionX;
-            }
+            objectPos = item.transform.position;
+            item.transform.SetParent(null);
+            item.GetComponent<Rigidbody>().useGravity = true;
         }
-
-        if (item.name == "Top Support Rod") {
-            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Clamp(transform.position.z, 28.4f, 37.6f));
-        }
-        if (item.name == "Bottom Support Rod") {
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, 74.55f, 83.95f), transform.position.y, transform.position.z);
-        }
-        wasHolding = isHolding;
 	}
     void carrying(GameObject o)
     {
